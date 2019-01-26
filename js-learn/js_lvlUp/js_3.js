625212758098161914e4ddaa8c73af3aea4c47c8
n.ronzin <nexux93@yandex.ru>
2019-01-27 01:00:29 +0300

    Сделана простая валидация полей инпут для имени, телефона и email через регулярные выражения согласно дз 3

--- js-learn/js_lvlUp/js_3.js
+++ js-learn/js_lvlUp/js_3.js
@@ -1,5 +1,63 @@
-var b = "fues psefn\'sgesgf sas\' sgbbg seg sef \'efswef\'";
+var b = "fues psefn' sgesgf sas'e sgbbg seg  ' sef 'efswef'";
 
 
-console.log(b.replace(/'/g, "\""));
+// console.log(b.replace(/'/g, "\""));
 
+// console.log(b.replace(/[^]'/g, "\""));
+window.onload = function () {
+    var name = document.getElementById('name');
+    var tel = document.getElementById('tel');
+    var email = document.getElementById('email');
+    var text = document.getElementById('text');
+    var butt = document.getElementById('submit');
+
+
+    butt.addEventListener("click", getValidate);
+
+
+    function getValidate() {
+        if ( /^[a-zа-яё]+$/i.test(name.value) == true) {
+            if(name.classList == 'denied') {
+                name.classList.remove('denied')
+            }
+            name.textContent = 'верно';
+            name.classList.add('sucsess');
+
+            console.log('Имя прошло валидацию');
+        } else {
+            if(name.classList == 'sucsess') {
+                name.classList.remove('sucsess')
+            }
+            name.textContent = 'не верно';
+            name.classList.add('denied');
+        }
+
+        if (/\+\d\(\d\d\d\)\d\d\d\-\d\d\d\d/g.test(tel.value) == true ) {
+            if(tel.classList == 'denied') {
+                tel.classList.remove('denied')
+            }
+            tel.textContent = 'верно';
+            tel.classList.add('sucsess');
+            console.log('Телефон прошёл валидацию');
+        } else {
+            if(tel.classList == 'sucsess') {
+                tel.classList.remove('sucsess')
+            }
+            tel.textContent = 'не верно';
+            tel.classList.add('denied');
+        }
+
+        if (/^[-._a-z0-9]+@(?:[a-z0-9][-a-z0-9]+\.)+[a-z]{2,6}/.test(email.value ) == true) {
+            if(email.classList == 'denied') {
+                email.classList.remove('denied')
+            }
+            email.classList.add('sucsess');
+            console.log('Email прошёл валидацию');
+        } else {
+            if(email.classList == 'sucsess') {
+                email.classList.remove('sucsess')
+            }
+            email.classList.add('denied');
+        }
+    }
+};
