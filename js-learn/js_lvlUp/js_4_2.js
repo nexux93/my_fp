46a3b4346cde64c529542f14bf6a17097e6e7ab4
n.ronzin <nexux93@yandex.ru>
2019-01-28 22:05:09 +0300

    Добавлено задание 2, с выпадающим списокм городов.

--- js-learn/js_lvlUp/js_4_2.js
+++ js-learn/js_lvlUp/js_4_2.js
@@ -1,15 +1,9 @@
-var b = "fues psefn' sgesgf sas'e sgbbg seg  ' sef 'efswef'";
-
-
-// console.log(b.replace(/'/g, "\""));
-
-// console.log(b.replace(/[^]'/g, "\""));
-
 window.onload = function () {
     var name = document.getElementById('name');
     var tel = document.getElementById('tel');
     var email = document.getElementById('email');
     var text = document.getElementById('text');
+    var town = document.getElementById('town');
     var butt = document.getElementById('submit');
 
 
@@ -17,8 +11,8 @@ window.onload = function () {
 
 
     function getValidate() {
-        if ( /^[a-zа-яё]+$/i.test(name.value) == true) {
-            if(name.classList == 'denied') {
+        if (/^[a-zа-яё]+$/i.test(name.value) == true) {
+            if (name.classList == 'denied') {
                 name.classList.remove('denied')
             }
             name.textContent = 'верно';
@@ -26,39 +20,57 @@ window.onload = function () {
 
             console.log('Имя прошло валидацию');
         } else {
-            if(name.classList == 'sucsess') {
+            if (name.classList == 'sucsess') {
                 name.classList.remove('sucsess')
             }
             name.textContent = 'не верно';
             name.classList.add('denied');
         }
 
-        if (/\+\d\(\d\d\d\)\d\d\d\-\d\d\d\d/g.test(tel.value) == true ) {
-            if(tel.classList == 'denied') {
+        if (/\+\d\(\d\d\d\)\d\d\d\-\d\d\d\d/g.test(tel.value) == true) {
+            if (tel.classList == 'denied') {
                 tel.classList.remove('denied')
             }
             tel.textContent = 'верно';
             tel.classList.add('sucsess');
             console.log('Телефон прошёл валидацию');
         } else {
-            if(tel.classList == 'sucsess') {
+            if (tel.classList == 'sucsess') {
                 tel.classList.remove('sucsess')
             }
             tel.textContent = 'не верно';
             tel.classList.add('denied');
         }
 
-        if (/^[-._a-z0-9]+@(?:[a-z0-9][-a-z0-9]+\.)+[a-z]{2,6}/.test(email.value ) == true) {
-            if(email.classList == 'denied') {
+        if (/^[-._a-z0-9]+@(?:[a-z0-9][-a-z0-9]+\.)+[a-z]{2,6}/.test(email.value) == true) {
+            if (email.classList == 'denied') {
                 email.classList.remove('denied')
             }
             email.classList.add('sucsess');
             console.log('Email прошёл валидацию');
         } else {
-            if(email.classList == 'sucsess') {
+            if (email.classList == 'sucsess') {
                 email.classList.remove('sucsess')
             }
             email.classList.add('denied');
         }
     }
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'js_lvlUp/planet_list.json');
+    xhr.send();
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            var catchCity = JSON.parse(xhr.responseText);
+
+            catchCity.forEach(function (catchCity) {
+                var opt = document.createElement('option');
+                opt.value = catchCity.planet;
+                opt.textContent = catchCity.planet;
+
+                town.appendChild(opt);
+
+            })
+        }
+    };
 };
