bbf08b7d43849e4a7212a8cb367ac26da703d2e5
n.ronzin <nexux93@yandex.ru>
2019-02-02 00:32:16 +0300

    Добавлен метод удаления отзыва и одобрения отзыва

--- js-learn/js_lvlUp/response_modul.js
+++ js-learn/js_lvlUp/response_modul.js
@@ -3,42 +3,86 @@
     var $responseList = $('.response');
     var $buttonSub = $('#submit');
     //add new response
-$($buttonSub).click(function() {
-    var $nameInput = $('#name').val();
-    var $textInput = $('#text').val();
-    $.ajax({
-        url: 'http://localhost:3000/response',
-        type: "POST",
-        dataType: 'json',
-        data: {
-            user: $nameInput,
-            response: $textInput
-        },
-        success: function () {
-alert('ваш отзыв отправлен на модерацию');
-        },
-        error: function (){
-            alert('Произошлой не предвиденный сбой');
-        }
-    })
-});
+    $($buttonSub).click(function () {
+        var $nameInput = $('#name').val();
+        var $textInput = $('#text').val();
+        $.ajax({
+            url: 'http://localhost:3000/response',
+            type: "POST",
+            dataType: 'json',
+            data: {
+                user: $nameInput,
+                response: $textInput
+            },
+            success: function () {
+                alert('ваш отзыв отправлен на модерацию');
+            },
+            error: function () {
+                alert('Произошёл не предвиденный сбой');
+            }
+        })
+    });
 
 // render all response
-$('#refresh').click(function () {
-    $.ajax({
-        url: 'http://localhost:3000/response',
-        type: "GET",
-        success: function (resp) {
-            var $ul = $('<ul>');
+    $('#refresh').click(getResponse);
 
-            resp.forEach(function(resp){
-                var $li = $('<li/>').text(resp.user + '(' + resp.response + ')');
-                $($ul).append($li);
-            });
+    function getResponse() {
+        $('.response').empty();
+        $.ajax({
+            url: 'http://localhost:3000/response',
+            type: "GET",
+            success: function (resp) {
+                var $ul = $('<ul>');
 
-            $('.response').append($ul);
+                resp.forEach(function (resp) {
+                    var $li = $('<li/>').text(resp.user + '(' + resp.response + ')');
+                    var $butt = $('<button/>').data(resp).addClass('Success').text('Одобрить').click(approvedResponse);
+                    var $dellResp = $('<button/>').data(resp).addClass('delete').text('Удалить').click(deleteResponse);
+                    $($li).append($butt);
+                    $($li).append($dellResp);
+                    $($ul).append($li);
+                });
 
-        }
-    })
-})
+                $('.response').append($ul);
+
+            }
+        })
+    }
+
+
+    function deleteResponse() {
+        var resp = $(this).data();
+
+        $.ajax({
+            url: 'http://localhost:3000/response/' + resp.id,
+            type: 'DELETE',
+            success: function() {
+                alert('Отзыв № ' +resp.id + ' удалён');
+                getResponse();
+            },
+            error: function () {
+                alert('Ошибка удаления отзыва');
+            }
+        })
+    }
+function approvedResponse() {
+        console.log(111);
+        var resp = $(this).data();
+
+        $.ajax({
+            url: 'http://localhost:3000/response/' + resp.id,
+            type: 'PATCH',
+            dataType: 'json',
+            data: {moder: "approved"},
+            success: function () {
+                alert('Отзыв одобрен');
+            },
+            error: function () {
+                alert('Внутренняя ошибка');
+            }
+        })
+
+    }
+
+    getResponse();
 })(jQuery);
