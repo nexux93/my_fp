afde37623cf4774c687debf2011785e6a30a8530
n.ronzin <nexux93@yandex.ru>
2019-02-08 20:54:06 +0300

    Валидация переписана на JQ

--- js-learn/js_lvlUp/js_6.js
+++ js-learn/js_lvlUp/js_6.js
@@ -1,18 +1,61 @@
 (function ($) {
-
-var name = $('#name');
+let name = $('#names');
 var tel = $('#tel');
 var email = $('#email');
 var text = $('#text');
-var town = $('#town');
-
 var butt = $('#submit').click(getValidate);
+var nameReg = /^[a-zа-яё]+$/i;
+var emailReg  = /^[-._a-z0-9]+@(?:[a-z0-9][-a-z0-9]+\.)+[a-z]{2,6}/;
+var telReg = /\+\d\(\d\d\d\)\d\d\d\-\d\d\d\d/g;
+
+function getValidate() {
+    if (nameReg.test(name.val())) {
+        if (name.attr('class') == 'denied') {
+            name.removeClass('denied')
+        }
+        name.addClass('sucsess');
+        console.log('Имя прошло валидацию');
+    } else {
+        if (name.attr('class') == 'sucsess') {
+            name.removeClass('sucsess')
+        }
+        name.textContent = 'не верно';
+        name.addClass('denied');
+    }
+
+    if (telReg.test(tel.val())) {
+        if (tel.attr('class') == 'denied') {
+            tel.removeClass('denied')
+        }
+        tel.textContent = 'верно';
+        tel.addClass('sucsess');
+        console.log('Телефон прошёл валидацию');
+    } else {
+        if (tel.attr('class') == 'sucsess') {
+            tel.removeClass('sucsess')
+        }
+        tel.textContent = 'не верно';
+        tel.addClass('denied');
+    }
+
+    if (emailReg.test(email.val())) {
+        if (email.attr('class') == 'denied') {
+            email.removeClass('denied');
+        }
+        email.addClass('sucsess');
+        console.log('Email прошёл валидацию');
+    } else {
+        if (email.attr('class') == 'sucsess') {
+            email.removeClass('sucsess')
+        }
+        email.addClass('denied');
+    }
+}
 
     $('#birthday').datepicker({
         buttonText: "Календарь",
         dateFormat: "dd-mm-yy",
         firstDay: 1,
-        dayNames: ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"],
         monthNames: [
             "Январь",
             "Февраль",
@@ -26,54 +69,17 @@ var butt = $('#submit').click(getValidate);
             "Октябрь",
             "Ноябрь",
             "Декабрь"
+        ],
+        dayNames: [
+            "пн",
+            "вт",
+            "ср",
+            "чт",
+            "пт",
+            "сб",
+            "вс"
         ]
     });
 
-function getValidate() {
-    if (/^[a-zа-яё]+$/i.test(name.value) == true) {
-        if (name.classList == 'denied') {
-            name.classList.remove('denied')
-        }
-        name.textContent = 'верно';
-        name.classList.add('sucsess');
 
-        console.log('Имя прошло валидацию');
-    } else {
-        if (name.classList == 'sucsess') {
-            name.classList.remove('sucsess')
-        }
-        name.textContent = 'не верно';
-        name.classList.add('denied');
-    }
-
-    if (/\+\d\(\d\d\d\)\d\d\d\-\d\d\d\d/g.test(tel.value) == true) {
-        if (tel.classList == 'denied') {
-            tel.classList.remove('denied')
-        }
-        tel.textContent = 'верно';
-        tel.classList.add('sucsess');
-        console.log('Телефон прошёл валидацию');
-    } else {
-        if (tel.classList == 'sucsess') {
-            tel.classList.remove('sucsess')
-        }
-        tel.textContent = 'не верно';
-        tel.classList.add('denied');
-    }
-
-    if (/^[-._a-z0-9]+@(?:[a-z0-9][-a-z0-9]+\.)+[a-z]{2,6}/.test(email.value) == true) {
-        if (email.classList == 'denied') {
-            email.classList.remove('denied')
-        }
-        email.classList.add('sucsess');
-        console.log('Email прошёл валидацию');
-    } else {
-        if (email.classList == 'sucsess') {
-            email.classList.remove('sucsess')
-        }
-        email.classList.add('denied');
-    }
-
-
-}
 })(jQuery);
