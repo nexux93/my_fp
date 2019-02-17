9225a89d1f6440c88e5cf19603311d16fde83c8e
Nikita Ronzin <nexux93@yandex.ru>
2019-02-18 01:57:45 +0300

    стрелки кастомизированы через костыль

--- single.js
+++ single.js
@@ -9,9 +9,9 @@ $(function() {
         navContainer: '#nav-carousel',
         navText: [
             // предыдущаяя
-            "<img src='img/ArrowPrew.svg' alt=''>",
+            "<div style='width: 50px;height: 50px; background-color: #5d5b5b;'><img src='img/ArrowPrew.svg' alt=''></div>",
             // следующаяя
-            "<img src='img/ArrowNext.svg' alt=''>",
+            "<div style='width: 50px;height: 50px; background-color: #5d5b5b;'><img src='img/ArrowNext.svg' alt=''></div>",
         ]
     });
 });
