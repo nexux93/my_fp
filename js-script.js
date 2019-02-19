664ddd1a9a0685ff52ea5941e6c347c3c31a2366
n.ronzin <nexux93@yandex.ru>
2019-02-20 00:09:31 +0300

    На странице Checkout сделан переход между вкладками пока через drop(up).

--- js-script.js
+++ js-script.js
@@ -14,7 +14,25 @@
         }, function () {
             $(this).attr('src', 'img/Forma_1.svg');
         });
-        $('ul.menu').hover()
+        $('ul.menu').hover();
+
+        // checkout page script
+            $('#check1').click(function () {
+                $(this).children().addClass('check-active');
+                $('#check2').children().removeClass('check-active');
+            });
+            $('#check2').click(function () {
+                $(this).children().addClass('check-active');
+                $('#check1').children().removeClass('check-active');
+            });
+            $('.list_left__next').click(function () {
+                $(this).parent().parent().toggle('drop', {direction: 'up'}).parent().next().children('.check_list__container').toggle('drop', {direction: 'up'});
+            });
+            $('.list_left__back').click(function() {
+                $(this).parent().parent().toggle('drop').parent().prev().children('.check_list__container').toggle('drop', {
+                    direction: 'up'
+                });
+            });
     });
 
 })(jQuery);
