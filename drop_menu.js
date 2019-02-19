957a49ba4f46d48422ee81916607d4eee1ff7951
n.ronzin <nexux93@yandex.ru>
2019-02-20 00:20:48 +0300

    В toggle drop UP заменены на default. Создан общий для всех шаблонов файл, содержащий (в будущем выпадающие менюшки)

---
+++ drop_menu.js
@@ -0,0 +1,3 @@
+(function ($) {
+    $('.btn_browse')
+})(jQuery);
--- js-script.js
+++ js-script.js
@@ -17,22 +17,22 @@
         $('ul.menu').hover();
 
         // checkout page script
-            $('#check1').click(function () {
-                $(this).children().addClass('check-active');
-                $('#check2').children().removeClass('check-active');
-            });
-            $('#check2').click(function () {
-                $(this).children().addClass('check-active');
-                $('#check1').children().removeClass('check-active');
-            });
-            $('.list_left__next').click(function () {
-                $(this).parent().parent().toggle('drop', {direction: 'up'}).parent().next().children('.check_list__container').toggle('drop', {direction: 'up'});
-            });
-            $('.list_left__back').click(function() {
-                $(this).parent().parent().toggle('drop').parent().prev().children('.check_list__container').toggle('drop', {
-                    direction: 'up'
-                });
-            });
+        // radiobutton check
+        $('#check1').click(function () {
+            $(this).children().addClass('check-active');
+            $('#check2').children().removeClass('check-active');
+        });
+        $('#check2').click(function () {
+            $(this).children().addClass('check-active');
+            $('#check1').children().removeClass('check-active');
+        });
+        // show and hide content in .check_list__container
+        $('.list_left__next').click(function () {
+            $(this).parent().parent().toggle('drop').parent().next().children('.check_list__container').toggle('drop');
+        });
+        $('.list_left__back').click(function () {
+            $(this).parent().parent().toggle('drop').parent().prev().children('.check_list__container').toggle('drop');
+        });
     });
 
 })(jQuery);
