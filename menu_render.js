7cb679be25a131d71c808784b8b3355c6b9453d5
n.ronzin <nexux93@yandex.ru>
2019-02-21 20:52:28 +0300

    добавил комметарии к коду отображения класса

--- menu_render.js
+++ menu_render.js
@@ -3,10 +3,12 @@
         var $mList = $('.menu_list');
 
         $($mList).mouseover(function () {
-            // console.log(e);
-  $('#Menu li').find('.mega_menu_container').removeClass('flex');
+//удаляем flex у всех потомков Menu
+            $('#Menu li').find('.mega_menu_container').removeClass('flex');
+            // добавляем контекстному потомку класс flex
             $(this).parent().children('.mega_menu_container').addClass('flex');
 
         });
+
     });
 })(jQuery);
