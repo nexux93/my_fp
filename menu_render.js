04209ebf6b0b0fb227ed6edb169044f90e6fd65f
n.ronzin <nexux93@yandex.ru>
2019-02-21 22:53:27 +0300

    Окончательно переписал появление меню c помощью css

--- menu_render.js
+++ menu_render.js
@@ -1,14 +1,8 @@
 (function ($) {
     $().ready(function () {
-        var $mList = $('.menu_list');
-
-        $($mList).mouseover(function () {
-//удаляем flex у всех потомков Menu
-            $('#Menu li').find('.mega_menu_container').removeClass('flex');
-            // добавляем контекстному потомку класс flex
-            $(this).parent().children('.mega_menu_container').addClass('flex');
-
+        $('#menu').menu('position', {
+            my: 'top: 33px',
+            left: '5.4688px'
         });
-
     });
 })(jQuery);
